45511e052a4e4bd09a56f9e87d9a6b41a90d8e80
Sipho <siphosethu@allaboutapps.co.za>
2021-05-14 07:32:34 +0200

    no message

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import './index.css';
-import App from './App';
+import Game from './Game';
 
 
 
 
 ReactDOM.render(
-    <App />,
+    <Game />,
     document.getElementById('root')
 );
 
