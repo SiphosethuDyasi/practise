11304d41174561c66484a764495d13d599e665c5
Sipho <siphosethu@allaboutapps.co.za>
2021-05-16 10:45:10 +0200

    commit to display the proper winner (there was an error)

--- src/Game.js
+++ src/Game.js
@@ -82,7 +82,7 @@ class Game extends React.Component {
 
         let status;
         if (winner) {
-            status = "Winner: " + winner;
+            status = "Winner: " + winner.player;
         } else {
             status = "Next player: " + (this.state.xIsNext ? "X" : "O");
         }
